1eab1ef822689702f19b99aa98778dda16d4588d
Kevin Vecchione <k.vecchione@me.com>
2024-11-16 17:28:43 -0500

    Use signals for transaction add/delete

--- savings_tracker/apps.py
+++ savings_tracker/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "savings_tracker"
+
+    def ready(self):
+        import savings_tracker.signals
