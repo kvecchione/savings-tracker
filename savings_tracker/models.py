f241161b9641382f203d11f519fef41e6e9f61f4
Kevin Vecchione <k.vecchione@me.com>
2026-02-19 21:41:57 -0500

    fix delete bug

--- savings_tracker/models.py
+++ savings_tracker/models.py
@@ -12,7 +12,7 @@ class Account(models.Model):
 
 class Transaction(models.Model):
     description = models.CharField(max_length=64)
-    account = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=50, decimal_places=2)
     datetime = models.DateTimeField()
     post_balance = models.DecimalField(max_digits=50, decimal_places=2, default=0)
@@ -22,7 +22,7 @@ class Transaction(models.Model):
     
 class ScheduledTransfer(models.Model):
     description = models.CharField(max_length=64)
-    account = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=50, decimal_places=2)
     day_of_month = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(28)])
 
