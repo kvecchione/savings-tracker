5033cc3d819566330074df15f7ffb2dd780b68b4
Kevin Vecchione <k.vecchione@me.com>
2026-02-19 21:29:07 -0500

    fix scheduled admin

--- savings_tracker/admin.py
+++ savings_tracker/admin.py
@@ -9,6 +9,12 @@ class AccountAdmin(admin.ModelAdmin):
 class TransactionAdmin(admin.ModelAdmin):
     readonly_fields = ('amount','post_balance','account')
 
+# ScheduledTransfer admin configuration
+class ScheduledTransferAdmin(admin.ModelAdmin):
+    list_display = ('description', 'account', 'amount', 'day_of_month')
+    list_filter = ('account',)
+    search_fields = ('description',)
+
 admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Account, AccountAdmin)
-admin.site.register(ScheduledTransfer)
+admin.site.register(ScheduledTransfer, ScheduledTransferAdmin)
